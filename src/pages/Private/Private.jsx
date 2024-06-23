35008612f8dffb32089386f07f5ccf9d671544f5
mariambuzaladze <mariambuzaladze10@example.come>
2024-06-24 00:14:33 +0400

    style: style private page

--- src/pages/Private/Private.jsx
+++ src/pages/Private/Private.jsx
@@ -1,15 +1,19 @@
 import { Link } from "react-router-dom";
 import Header from "./components/Header";
 import GoBack from "../../shared-components/GoBack";
+import Main from "./components/Main";
 
 export default function Private() {
   return (
-    <div className="flex">
+    <div className="flex bg-[#F9F9F9] h-screen p-4 gap-2">
       <GoBack />
-      <Header />
-      <Link to={"/experience"}>
-        <button>შემდეგი</button>
-      </Link>
+      <div className="flex flex-col gap-4 w-full">
+        <Header />
+        <Main />
+        <Link to={"/experience"}>
+          <button>შემდეგი</button>
+        </Link>
+      </div>
     </div>
   );
 }
