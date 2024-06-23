0695a9393acf1c9284a315bec66ac86e02987104
mariambuzaladze <mariambuzaladze10@example.come>
2024-06-23 23:59:41 +0400

    feat: add line component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
+import Line from "../../shared-components/Line";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-4 pt-3 p-4 h-screen">
       <Header />
-      <div className="h-[1px] bg-[#1A1A1A]"></div>
+      <Line />
       <Main />
     </div>
   );
--- src/pages/Private/components/Header.jsx
+++ src/pages/Private/components/Header.jsx
@@ -1,9 +1,11 @@
 import GoBack from "../../../shared-components/GoBack";
+import Line from "../../../shared-components/Line";
 
 export default function Header() {
   return (
     <header>
       <GoBack />
+      <Line />
     </header>
   );
 }
