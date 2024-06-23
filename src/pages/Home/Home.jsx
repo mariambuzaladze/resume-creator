f294ecb82190625ec63a2463f02bc7f1ec17ef47
mariambuzaladze <mariambuzaladze10@example.come>
2024-06-23 23:45:44 +0400

    feat: add main component for home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,12 @@
-import { Link } from "react-router-dom";
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-4 pt-3 p-4 h-screen">
       <Header />
-      <div className="w-screen h-[1px] bg-[#1A1A1A]"></div>
-      <Link to={"/private"}>
-        <button>რეზიუმეს დამატება</button>
-      </Link>
+      <div className="h-[1px] bg-[#1A1A1A]"></div>
+      <Main />
     </div>
   );
 }
