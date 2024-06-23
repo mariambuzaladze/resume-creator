681fb380816903861b4baefaa33245e6cfcdd3e8
mariambuzaladze <mariambuzaladze10@example.come>
2024-06-24 00:07:28 +0400

    feat: add header for private page

--- src/pages/Private/Private.jsx
+++ src/pages/Private/Private.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
 import Header from "./components/Header";
+import GoBack from "../../shared-components/GoBack";
 
 export default function Private() {
   return (
-    <div>
+    <div className="flex">
+      <GoBack />
       <Header />
       <Link to={"/experience"}>
         <button>შემდეგი</button>
--- src/pages/Private/components/Header.jsx
+++ src/pages/Private/components/Header.jsx
@@ -1,11 +1,15 @@
-import GoBack from "../../../shared-components/GoBack";
 import Line from "../../../shared-components/Line";
 
 export default function Header() {
   return (
-    <header>
-      <GoBack />
-      <Line />
+    <header className="flex">
+      <div className="flex flex-col gap-3">
+        <div className="flex justify-between">
+          <h1 className="font-bold text-lg">პირადი ინფო</h1>
+          <p>1/3</p>
+        </div>
+        <Line />
+      </div>
     </header>
   );
 }
