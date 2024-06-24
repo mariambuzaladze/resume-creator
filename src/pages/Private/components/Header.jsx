edeb1baaa271e3176764b597a9f049802fef1dd3
mariambuzaladze <mariambuzaladze10@example.come>
2024-06-25 00:09:04 +0400

    style: add styles for private info page

--- src/pages/Private/components/Header.jsx
+++ src/pages/Private/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header className="flex">
       <div className="flex flex-col gap-3 w-full">
         <div className="flex justify-between">
-          <h1 className="font-bold text-lg">პირადი ინფო</h1>
+          <h1 className="font-bold text-lg md:text-xl">პირადი ინფო</h1>
           <p>1/3</p>
         </div>
         <Line />
--- src/pages/Private/components/Main.jsx
+++ src/pages/Private/components/Main.jsx
@@ -3,6 +3,10 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
 export default function Main() {
+  const handleButtonClick = () => {
+    document.getElementById("image").click();
+  };
+
   const initialValues = {};
 
   const validationSchema = Yup.object().shape({});
@@ -17,96 +21,126 @@ export default function Main() {
         onSubmit={handleSubmit}
       >
         {({ errors, touched }) => (
-          <Form className="flex flex-col justiy-center items-centerh-screen gap-4">
-            <div>
-              <label htmlFor="name" className="font-bold block mb-2">
-                სახელი
-              </label>
-              <Field
-                type="text"
-                name="name"
-                placeholder="ანზორ"
-                className="border border-gray-300 rounded-lg p-2"
-              />
-              <ErrorMessage
-                name="name"
-                component="p"
-                className="text-red-500 text-sm"
-              />
+          <Form className="flex flex-col justiy-center items-centerh-screen gap-4 md:gap-6">
+            <div className="flex flex-col md:flex-row justify-between">
+              <div>
+                <label htmlFor="name" className="font-bold block mb-2">
+                  სახელი
+                </label>
+                <Field
+                  type="text"
+                  name="name"
+                  placeholder="ანზორ"
+                  className="border border-gray-300 rounded-lg p-2 w-full"
+                />
+                <ErrorMessage
+                  name="name"
+                  component="p"
+                  className="text-red-500 text-sm"
+                />
+              </div>
 
-              <label htmlFor="surname" className="font-bold block mb-2">
-                გვარი
+              <div>
+                <label htmlFor="surname" className="font-bold block mb-2">
+                  გვარი
+                </label>
+                <Field
+                  type="text"
+                  name="surname"
+                  placeholder="მუმლაძე"
+                  className="border border-gray-300 rounded-lg p-2 w-full"
+                />
+                <ErrorMessage
+                  name="surname"
+                  component="p"
+                  className="text-red-500 text-sm"
+                />
+              </div>
+            </div>
+
+            <div>
+              <label htmlFor="image" className="font-bold block mb-2">
+                პირადი ფოტოს ატვირთვა
               </label>
-              <Field
-                type="text"
-                name="surname"
-                placeholder="მუმლაძე"
-                className="border border-gray-300 rounded-lg p-2"
-              />
+              <div className="custom-file-input ">
+                <Field
+                  id="image"
+                  type="file"
+                  accept="image/*"
+                  name="image"
+                  placeholder="ატვირთვა"
+                  className="hidden"
+                />
+                <button
+                  type="button"
+                  className="custom-button"
+                  onClick={handleButtonClick}
+                >
+                  ატვირთვა
+                </button>
+              </div>
               <ErrorMessage
-                name="surname"
+                name="image"
                 component="p"
                 className="text-red-500 text-sm"
               />
             </div>
 
-            <label htmlFor="image" className="font-bold block mb-2">
-              პირადი ფოტოს ატვირთვა
-            </label>
-            <Field type="file" accept="image/*" name="image" />
-            <ErrorMessage
-              name="image"
-              component="p"
-              className="text-red-500 text-sm"
-            />
+            <div>
+              <label htmlFor="aboutMe" className="font-bold block mb-2">
+                ჩემ შესახებ (არასავალდებულო)
+              </label>
+              <Field
+                as="textarea"
+                name="aboutMe"
+                placeholder="ზოგადი ინფო შენ შესახებ"
+                className="p-2 border border-gray-300 rounded-lg w-full"
+              />
+              <ErrorMessage
+                name="aboutMe"
+                component="p"
+                className="text-red-500 text-sm"
+              />
+            </div>
 
-            <label htmlFor="aboutMe" className="font-bold block mb-2">
-              ჩემ შესახებ (არასავალდებულო)
-            </label>
-            <Field
-              as="textarea"
-              name="aboutMe"
-              placeholder="ზოგადი ინფო შენ შესახებ"
-              className="p-2 border border-gray-300 rounded-lg"
-            />
-            <ErrorMessage
-              name="aboutMe"
-              component="p"
-              className="text-red-500 text-sm"
-            />
+            <div>
+              <label htmlFor="email" className="font-bold block mb-2">
+                ელ.ფოსტა
+              </label>
+              <Field
+                type="email"
+                name="email"
+                placeholder="anzorr666@redberry.ge"
+                className="border border-gray-300 rounded-lg p-2 w-full"
+              />
+              <ErrorMessage
+                name="email"
+                component="p"
+                className="text-red-500 text-sm"
+              />
+            </div>
 
-            <label htmlFor="email" className="font-bold block mb-2">
-              ელ.ფოსტა
-            </label>
-            <Field
-              type="email"
-              name="email"
-              placeholder="anzorr666@redberry.ge"
-              className="border border-gray-300 rounded-lg p-2"
-            />
-            <ErrorMessage
-              name="email"
-              component="p"
-              className="text-red-500 text-sm"
-            />
+            <div>
+              <label htmlFor="number" className="font-bold block mb-2">
+                მობილურის ნომერი
+              </label>
+              <Field
+                type="number"
+                name="number"
+                placeholder="+995 551 12 34 56"
+                className="border border-gray-300 rounded-lg p-2 w-full"
+              />
+              <ErrorMessage
+                name="number"
+                component="p"
+                className="text-red-500 text-sm"
+              />
+            </div>
 
-            <label htmlFor="number" className="font-bold block mb-2">
-              მობილურის ნომერი
-            </label>
-            <Field
-              type="number"
-              name="number"
-              placeholder="+995 551 12 34 56"
-              className="border border-gray-300 rounded-lg p-2"
-            />
-            <ErrorMessage
-              name="number"
-              component="p"
-              className="text-red-500 text-sm"
-            />
-
-            <Link to={"/experience"}>
-              <button>შემდეგი</button>
+            <Link to={"/experience"} className="flex justify-end">
+              <button className="text-white bg-[#6B40E3] rounded-sm pt-1 pb-1 p-4 mt-10 mb-4 md:mb-8">
+                შემდეგი
+              </button>
             </Link>
           </Form>
         )}
