9baedb27cf93058c00e8695e59bdbbcd434942ec
lashasaata <lashasaata@gmail.com>
2024-06-25 05:43:56 -0700

    feat: add go back component

--- src/pages/Education/Education.jsx
+++ src/pages/Education/Education.jsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom";
 import Educate from "./components/Educate";
+import GoBack from "../../shared-components/GoBack";
 
 function Education() {
   return (
-    <main className="bg-[#f9f9f9] min-h-svh">
+    <main className="bg-[#f9f9f9] min-h-svh relative">
+      <div className="absolute top-[45px] left-[48px]">
+        <GoBack />
+      </div>
       <section className="min-h-svh flex flex-col justify-between px-[150px] pt-[47px] pb-[65px]">
         <div>
           <header className="flex justify-between items-center pb-[11px] border-b-[2px] border-solid border-[#1a1a1a]">
