4aa06e3f3ab6fb057200e43572045e8b628c6051
mariambuzaladze <mariambuzaladze10@example.come>
2024-06-26 22:07:44 +0400

    style: add background image to home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,18 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Line from "../../shared-components/Line";
+import BackgroundImg from "/background.png";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-4 pt-3 p-4 h-screen">
+    <div
+      className="flex flex-col gap-4 pt-3 p-4 h-screen"
+      style={{
+        backgroundImage: `url(${BackgroundImg})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
       <Header />
       <Line />
       <Main />
