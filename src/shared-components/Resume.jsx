bf5522af489a7d68c394ce9a84a3d3b4dbca2799
lashasaata <lashasaata@gmail.com>
2024-06-29 08:13:35 -0700

    style: styles for genaral information section

--- src/shared-components/Resume.jsx
+++ src/shared-components/Resume.jsx
@@ -5,11 +5,11 @@ function Resume() {
   const context = useContext(dataContext);
 
   return (
-    <main className=" px-20 py-[44px]">
-      <section>
-        <aside>
-          <div>
-            <p>
+    <main className="w-[822px] bg-[#fff] flex flex-col justify-between px-20 py-[44px]">
+      <section className="flex justify-between pb-[18px] border-b border-solid border-[#c8c8c8]">
+        <aside className="flex flex-col gap-[34px] mt-6">
+          <div className="flex flex-col gap-[17px]">
+            <p className="text-[34px] text-[#f93b1d] font-[700] leading-[42px]">
               {context.data.general.name ? context.data.general.name : ""}
               <span>
                 {context.data.general.surname
@@ -17,22 +17,24 @@ function Resume() {
                   : ""}
               </span>
             </p>
-            <div>
-              <div>
+            <div className="flex flex-col gap-[10px]">
+              <div className="flex items-center gap-[10px]">
                 <img
                   src={context.data.general.email ? "/email.png" : ""}
                   alt="email"
+                  className="w-5 h-5"
                 />
-                <span>
+                <span className="text-lg text-[#1a1a1a] font-[400] leading-[21px]">
                   {context.data.general.email ? context.data.general.email : ""}
                 </span>
               </div>
-              <div>
+              <div className="flex items-center gap-[10px]">
                 <img
                   src={context.data.general.number ? "/phone.png" : ""}
                   alt="phone"
+                  className="w-5 h-5"
                 />
-                <span>
+                <span className="text-lg text-[#1a1a1a] font-[400] leading-[21px]">
                   {context.data.general.number
                     ? context.data.general.number
                     : ""}
@@ -40,9 +42,11 @@ function Resume() {
               </div>
             </div>
           </div>
-          <div>
-            <h2>ᲩᲔᲛ ᲨᲔᲡᲐᲮᲔᲑ</h2>
-            <p>
+          <div className="flex flex-col gap-4">
+            <h2 className="text-lg text-[#f93b1d] font-[700] leading-[22px]">
+              ᲩᲔᲛ ᲨᲔᲡᲐᲮᲔᲑ
+            </h2>
+            <p className="max-w-[432px] text-base text-[#000] font-[400] leading-[1.38]">
               {context.data.general.aboutMe ? context.data.general.aboutMe : ""}
             </p>
           </div>
@@ -50,9 +54,14 @@ function Resume() {
         <img
           //   src={context.data.general.image ? context.data.general.image : ""}
           alt="avatar"
+          className="w-[246] h-[246px] rounded-[133px]"
         />
       </section>
-      <img src="/redberryMiniLogo.png" alt="logo" />
+      <img
+        src="/redberryMiniLogo.png"
+        alt="logo"
+        className="w-[42px] h-[42px]"
+      />
     </main>
   );
 }
