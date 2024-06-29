6529aeb43730a8d125fa65f8ed88684af7d3cd76
lashasaata <lashasaata@gmail.com>
2024-06-29 11:23:53 -0700

    add resume component in experience page and modify styles for it

--- src/pages/Experience/Experience.jsx
+++ src/pages/Experience/Experience.jsx
@@ -2,15 +2,20 @@ import { Link } from "react-router-dom";
 import Header from "./components/Header";
 import GoBack from "../../shared-components/GoBack";
 import Main from "./components/Main";
+import Resume from "../../shared-components/Resume";
 
 export default function Experience() {
   return (
-    <div className="flex bg-[#F9F9F9] min-h-screen p-4 gap-2 md:gap-8 dark:bg-[#141625]">
-      <GoBack />
-
-      <div className="flex flex-col gap-4 w-full mr-6 md:gap-8">
-        <Header />
-        <Main />
+    <div className="flex bg-[#F9F9F9] min-h-screen gap-2 md:gap-8 dark:bg-[#141625] relative">
+      <div className="absolute top-[45px] left-[48px]">
+        <GoBack />
+      </div>
+      <div className="flex justify-between">
+        <div className="flex flex-col gap-4 w-full md:gap-8 px-[150px] pt-[47px]">
+          <Header />
+          <Main />
+        </div>
+        <Resume />
       </div>
     </div>
   );
