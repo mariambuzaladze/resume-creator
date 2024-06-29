f3742d9a680ad8df4310f2ec3a0b1c87528df047
lashasaata <lashasaata@gmail.com>
2024-06-29 10:49:46 -0700

    style-fix: change some styles in resume component

--- src/shared-components/Resume.jsx
+++ src/shared-components/Resume.jsx
@@ -9,7 +9,7 @@ function Resume() {
       <section className="flex justify-between pb-[18px] border-b border-solid border-[#c8c8c8]">
         <aside className="flex flex-col gap-[34px] mt-6">
           <div className="flex flex-col gap-[17px]">
-            <p className="text-[34px] text-[#f93b1d] font-[700] leading-[42px]">
+            <p className="max-w-[400px] h-[42px] flex items-center gap-5 text-[34px] text-[#f93b1d] font-[700] leading-[42px]">
               {context.data.general.name ? context.data.general.name : ""}
               <span>
                 {context.data.general.surname
@@ -18,23 +18,27 @@ function Resume() {
               </span>
             </p>
             <div className="flex flex-col gap-[10px]">
-              <div className="flex items-center gap-[10px]">
+              <div className="h-[21px] flex items-center gap-[10px]">
                 <img
-                  src={context.data.general.email ? "/email.png" : ""}
+                  src="/email.png"
                   alt="email"
-                  className="w-5 h-5"
+                  className={`${
+                    context.data.general.email ? "flex" : "hidden"
+                  } w-5 h-5`}
                 />
-                <span className="text-lg text-[#1a1a1a] font-[400] leading-[21px]">
+                <span className="max-w-[300px] text-lg text-[#1a1a1a] font-[400] leading-[21px]">
                   {context.data.general.email ? context.data.general.email : ""}
                 </span>
               </div>
-              <div className="flex items-center gap-[10px]">
+              <div className="h-[21px] flex items-center gap-[10px]">
                 <img
-                  src={context.data.general.number ? "/phone.png" : ""}
+                  src="/phone.png"
                   alt="phone"
-                  className="w-5 h-5"
+                  className={`${
+                    context.data.general.number ? "flex" : "hidden"
+                  } w-5 h-5`}
                 />
-                <span className="text-lg text-[#1a1a1a] font-[400] leading-[21px]">
+                <span className="max-w-[300px] text-lg text-[#1a1a1a] font-[400] leading-[21px]">
                   {context.data.general.number
                     ? context.data.general.number
                     : ""}
@@ -43,7 +47,11 @@ function Resume() {
             </div>
           </div>
           <div className="flex flex-col gap-4">
-            <h2 className="text-lg text-[#f93b1d] font-[700] leading-[22px]">
+            <h2
+              className={`${
+                context.data.general.aboutMe ? "flex" : "hidden"
+              } h-[22px] text-lg text-[#f93b1d] font-[700] leading-[22px]`}
+            >
               ᲩᲔᲛ ᲨᲔᲡᲐᲮᲔᲑ
             </h2>
             <p className="max-w-[432px] text-base text-[#000] font-[400] leading-[1.38]">
@@ -52,9 +60,11 @@ function Resume() {
           </div>
         </aside>
         <img
-          //   src={context.data.general.image ? context.data.general.image : ""}
+          src={context.data.general.image}
           alt="avatar"
-          className="w-[246] h-[246px] rounded-[133px]"
+          className={`${
+            context.data.general.image ? "flex" : "hidden"
+          } w-[246] h-[246px] rounded-[133px]`}
         />
       </section>
       <img
