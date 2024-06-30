b25ce6c43c666bc56f077b7ab5e42b07398ff12f
lashasaata <lashasaata@gmail.com>
2024-06-30 06:07:28 -0700

    feat: add close function on close icon

--- src/pages/ResumeM/components/Sent.jsx
+++ src/pages/ResumeM/components/Sent.jsx
@@ -1,10 +1,23 @@
+import { useState } from "react";
+
 function Sent() {
+  const [useSend, setUseSend] = useState(true);
+
+  const handleClose = () => {
+    setUseSend(false);
+  };
+
   return (
-    <div className="relative w-[427px] pl-[30px] pr-[33px] pt-[39.5px] pb-[41.5px] border border-solid border-[#e4e4e4] shadow-sent mr-20">
+    <div
+      className={`${
+        useSend ? "flex" : "hidden"
+      } relative w-[427px] pl-[30px] pr-[33px] pt-[39.5px] pb-[41.5px] border border-solid border-[#e4e4e4] shadow-sent mr-20 `}
+    >
       <img
         src="/icon-close.svg"
         alt="close"
-        className="absolute top-[14px] right-[8px]"
+        className="absolute top-[14px] right-[8px] hover:cursor-pointer"
+        onClick={handleClose}
       />
       <p className="text-[28px] text-[#1a1a1a] font-[500] leading-[1.54]">
         რეზიუმე წარმატებით გაიგზავნა 🎉
