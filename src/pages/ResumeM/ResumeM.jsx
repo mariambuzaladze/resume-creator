1b3d7cabf461c749c9367e971bb2bfc1dae4dfff
lashasaata <lashasaata@gmail.com>
2024-06-30 09:06:14 -0700

    feat: add function that do not let user to navigate on resume page without fill out forms

--- src/pages/ResumeM/ResumeM.jsx
+++ src/pages/ResumeM/ResumeM.jsx
@@ -1,8 +1,46 @@
+import { useContext, useEffect } from "react";
+import { dataContext } from "../../App";
+import { useNavigate } from "react-router-dom";
 import GoBack from "../../shared-components/GoBack";
 import Resume from "../../shared-components/Resume";
 import Sent from "./components/Sent";
 
 export default function ResumeM() {
+  const context = useContext(dataContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const { general, experience, education } = context.data;
+
+    if (
+      !general ||
+      !general.name ||
+      !general.surname ||
+      !general.image ||
+      !general.email ||
+      !general.number
+    ) {
+      navigate("/private");
+    } else if (
+      experience.some(
+        (exp) =>
+          !exp.position ||
+          !exp.employer ||
+          !exp.started_at ||
+          !exp.ended_at ||
+          !exp.description
+      )
+    ) {
+      navigate("/experience");
+    } else if (
+      education.some(
+        (edu) =>
+          !edu.school || !edu.degree || !edu.graduation_date || !edu.description
+      )
+    ) {
+      navigate("/education");
+    }
+  }, []);
   return (
     <main className="w-full flex justify-center pt-[54px] pb-[129px] relative">
       <div className="absolute top-[45px] left-[48px]">
