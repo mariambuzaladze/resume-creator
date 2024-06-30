1b568b37e642c613021e5d74afaf20367f4a3b8b
lashasaata <lashasaata@gmail.com>
2024-06-30 05:58:01 -0700

    add set notification component with styles and content

---
+++ src/pages/ResumeM/components/Sent.jsx
@@ -0,0 +1,16 @@
+function Sent() {
+  return (
+    <div className="relative w-[427px] pl-[30px] pr-[33px] pt-[39.5px] pb-[41.5px] border border-solid border-[#e4e4e4] shadow-sent mr-20">
+      <img
+        src="/icon-close.svg"
+        alt="close"
+        className="absolute top-[14px] right-[8px]"
+      />
+      <p className="text-[28px] text-[#1a1a1a] font-[500] leading-[1.54]">
+        რეზიუმე წარმატებით გაიგზავნა 🎉
+      </p>
+    </div>
+  );
+}
+
+export default Sent;
